340ff3b1c5d6559d3e679527d5d422340a9469d1
Mike <mbell85@ymail.com>
2026-02-20 11:43:53 +0000

    route and blueprint for home page created

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from app.routes.main import main_bp
 from config import Config
 from app.extensions import db, login_manager
+from app.models import User
 
 """
 here i am setting up the main Flask application, and the database tables that will
 be created (if they dont exist) on application run
+
+i have also registered the blueprints that will be used to manage my routes, and
+attached any prefixes they may need
 """
 
 def create_app():
@@ -14,12 +19,13 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
+    app.register_blueprint(main_bp)
+
     #import models here for db tables when created
     #register routes here when created
     #register the blueprints here when routes are created - and add prefixes here
 
-    #uncomment this when you have tables to make
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ app/models/__init__.py
@@ -0,0 +1,12 @@
+from app import login_manager
+from app.models.user import User
+
+
+"""
+here i make the relevant imports for my models to prevent having to make them repeatedly elsewhere
+and also set up the user loader
+"""
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
