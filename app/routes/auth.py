10ab04e43db22beb211abbc6403daced45c4af32
Mike <mbell85@ymail.com>
2026-02-21 13:47:51 +0000

    updated register route to account for new form

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 
-from app.forms.auth_forms import LoginUserForm
+from app.forms.auth_forms import LoginUserForm, RegisterUserForm
 
 """
 routes related to logging in and registering users
@@ -23,6 +23,15 @@ def login():
 
     return render_template("login.html")
 
-@auth_bp.route("/register")
+@auth_bp.route("/register", methods = ["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+        form = RegisterUserForm(request.form)
+
+        if not form.success:
+            for error in form.errors.values():
+                flash(error, "error")
+            return redirect(url_for("auth.login"))
+
     return render_template("register.html")
