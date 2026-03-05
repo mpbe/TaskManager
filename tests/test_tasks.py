936b4626618ae13db143d3a9a511ff6f70ea181d
Mike <mbell85@ymail.com>
2026-03-05 13:53:52 +0000

    update_task test added

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -10,7 +10,6 @@ def test_create_task_success(client):
     })
 
     response = client.post("/tasks/create", data={
-        "user_id": 100,
         "title": "test",
         "description": "test",
         "due_date": (date.today() + timedelta(days=3)).isoformat(),
@@ -20,6 +19,32 @@ def test_create_task_success(client):
     }, follow_redirects=True)
 
     assert b"task successfully created!" in response.data
+
+
+def test_update_task_success(client):
+    client.post("/auth/register", data={
+        "username": "test",
+        "email": "test@test.com",
+        "password": "test"
+    })
+
+    client.post("/tasks/create", data={
+        "title": "test",
+        "description": "test",
+        "due_date": (date.today() + timedelta(days=3)).isoformat(),
+        "status": Status.TO_DO.value,
+        "priority": Priority.LOW.value
+    })
+
+    response = client.post("/tasks/update/1", data={
+        "title": "test2",
+        "description": "test2",
+        "due_date": (date.today() + timedelta(days=5)).isoformat(),
+        "status": Status.IN_PROGRESS.value,
+        "priority": Priority.MEDIUM.value
+    }, follow_redirects=True)
+
+    assert b"task successfully updated!" in response.data
 
 
 def test_delete_task_success(client):
