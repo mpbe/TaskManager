3bc8b8aa8ce273125d025ccc7198ee52301d7b11
Mike <mbell85@ymail.com>
2026-03-06 14:40:23 +0000

    changed the seed to add more tasks and give more realistic descriptions

--- seed.py
+++ seed.py
@@ -38,7 +38,7 @@ with app.app_context():
 
     task1 = Task(
             title="First task",
-            description="Example task",
+            description="Lorem ipsum dolor sit amet consectetur adipiscing",
             due_date=date.today() + timedelta(days=7),
             status=Status.TO_DO,
             priority=Priority.LOW,
@@ -47,7 +47,7 @@ with app.app_context():
 
     task2 = Task(
             title="Second task",
-            description="Another task",
+            description="Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi.",
             due_date=date.today() + timedelta(days=3),
             status=Status.IN_PROGRESS,
             priority=Priority.HIGH,
@@ -72,7 +72,43 @@ with app.app_context():
             user_id=user2.id
         )
 
-    db.session.add_all([task1, task2, task3, task4])
+    task5 = Task(
+        title="Third task",
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
+        due_date=date.today() + timedelta(days=7),
+        status=Status.TO_DO,
+        priority=Priority.LOW,
+        user_id=user1.id
+    )
+
+    task6 = Task(
+        title="Fourth task",
+        description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' ",
+        due_date=date.today() + timedelta(days=3),
+        status=Status.IN_PROGRESS,
+        priority=Priority.HIGH,
+        user_id=user1.id
+    )
+
+    task7 = Task(
+        title="Third task",
+        description="Example task",
+        due_date=date.today() + timedelta(days=7),
+        status=Status.TO_DO,
+        priority=Priority.LOW,
+        user_id=user2.id
+    )
+
+    task8 = Task(
+        title="Fourth task",
+        description="Another task",
+        due_date=date.today() + timedelta(days=3),
+        status=Status.IN_PROGRESS,
+        priority=Priority.HIGH,
+        user_id=user2.id
+    )
+
+    db.session.add_all([task1, task2, task3, task4, task5, task6, task7, task8])
     db.session.commit()
 
     print("Database seeded.")
